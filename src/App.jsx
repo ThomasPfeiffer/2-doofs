3ed0e800192e46078b4f0a27fc5ee3231d7b1774
Thomas Pfeiffer <git.ThomasPfeiffer@live.de>
2024-05-20 10:29:02 +0000

    Runden lsöchen

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useCallback, useState } from 'react';
 import './App.css';
 import { Button, Card, Container, IconButton, Stack, TextField, Box, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, CardContent, CardActions } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
-
+import DeleteIcon from '@mui/icons-material/Delete';
 function App() {
   const [players, setPlayers] = useLocalState("players", [])
   const [scores, setScores] = useLocalState("scores", [])
@@ -24,13 +24,15 @@ function App() {
         <CardContent sx={{
           p: 3, gap: 2,
           display: "grid",
-          gridTemplateColumns: `repeat(${players.length}, 1fr)`
+          gridTemplateColumns: `repeat(${players.length + 1}, 1fr)`
         }}>
           {players.map(p => <Box key={p}>{p} ({scores.reduce((acc, score) => acc + (score[p] ?? 0), 0)})</Box>)}
+          <span></span>
           {scores.map((score, index) => {
             return <React.Fragment key={index}>
               {players.map((player) => {
-                return <Box key={player}>
+                return <React.Fragment key={player}>
+                <Box >
                   <TextField value={score[player] ?? "0"} type="number" variant="filled" sx={{ width: 60 }}
                     onChange={event => {
                       const newValue = parseInt(event.target.valueAsNumber)
@@ -43,7 +45,13 @@ function App() {
                       }
                     }} />
                 </Box>
+                </React.Fragment>
               })}
+              <Box>
+              <DeleteScoreButton onReset={() => {
+                setScores(scores.filter((_, index2) => index !== index2))
+              }}/>
+              </Box>
             </React.Fragment>
           })}
         </CardContent>
@@ -97,6 +105,49 @@ function GigaWizard({ players, scores, setScores }) {
       }>{player} {player in scores ? `(${scores[player]})` : ""}</Button>
     })}
   </Stack>
+}
+
+
+function DeleteScoreButton({ onReset }) {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <React.Fragment>
+    <IconButton onClick={handleClickOpen}><DeleteIcon /></IconButton>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Ganz sicher?"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Runde löschen?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Nein</Button>
+          <Button onClick={() => {
+            onReset()
+            handleClose()
+          }} autoFocus>
+            Ja
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </React.Fragment>
+  );
 }
 
 function CompleteResetButton({ onReset }) {
